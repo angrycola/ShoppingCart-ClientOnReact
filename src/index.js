31b640f8e397544c2a90c64f8768f7b59cb61e14
angrycola <youagain@yandex.ru>
2017-04-17 13:05:02 +0300

    Up w Redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+
 import App from './components/App';
+import reducer from './reducers';
 import './styles/index.css';
 
+
+const store = createStore(
+  reducer,
+  composeWithDevTools(
+    applyMiddleware(thunk)
+  )
+);
+
+
 ReactDOM.render(
-  <App />,
+  <BrowserRouter>
+    <Provider store={ store }>
+      <App />
+    </Provider>
+  </BrowserRouter>,
   document.getElementById('root')
 );
