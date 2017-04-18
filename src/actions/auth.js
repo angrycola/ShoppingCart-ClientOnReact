3ae0312d9035d6a4a92d23a0c3be5f6575f44ff4
angrycola <youagain@yandex.ru>
2017-04-18 11:46:51 +0300

    SignOut

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -10,14 +10,20 @@ const baseURL = 'http://localhost:8080/users';
 export const setCurrentUser = user => ({ type: C.SET_CURRENT_USER, payload: user });
 
 export const signInRequest = userData => dispatch => axios.post(`${ baseURL }/signin`, userData)
-.then(res => {
-  const token = res.data.token;
-  localStorage.setItem('token', token);
+  .then(res => {
+    const token = res.data.token;
+    localStorage.setItem('token', token);
 
-  setAuthToken(token);
-  dispatch(setCurrentUser(jwtDecode(token)));
+    setAuthToken(token);
+    dispatch(setCurrentUser(jwtDecode(token)));
 });
 
 export const signUpRequest = userData => dispatch => axios.post(`${ baseURL }/signup`, userData);
 
 export const isUserExists = email => dispath => axios.post(`${ baseURL }/pre`, { email });
+
+export const signOutUser = () => dispatch => {
+  localStorage.removeItem('token');
+  setAuthToken(false);
+  dispatch(setCurrentUser({}));
+};
--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { Link, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { signOutUser } from '../actions/auth';
 import { Menu } from 'semantic-ui-react';
 import SignUpPage from './auth/SignUpPage';
 import SignInPage from './auth/SignInPage';
@@ -11,51 +14,72 @@ class NavMenu extends Component {
 
   handleClick = (event, { name }) => {
     this.setState({ activeItem: name });
+    this.props.signOutUser();
   }
 
   render() {
     const { activeItem } = this.state;
+    const { auth } = this.props;
+
+    const authed = (
+      <Menu.Menu position='right'>
+        <Menu.Item
+          as={ Link }
+          to='/signout'
+          name='signout'
+          active={ activeItem === 'signout' }
+          onClick={ this.handleClick }>
+            Sign Out
+        </Menu.Item>
+      </Menu.Menu>
+    );
+
+    const unauthed = (
+      <Menu.Menu position='right'>
+        <Menu.Item
+          as={ Link }
+          to='/signin'
+          name='signin'
+          active={ activeItem === 'signin' }
+          onClick={ this.handleClick }>
+            Sign In
+        </Menu.Item>
+
+        <Menu.Item
+          as={ Link }
+          to='/signup'
+          name='signup'
+          active={ activeItem === 'signup' }
+          onClick={ this.handleClick }>
+            Sign Up
+        </Menu.Item>
+      </Menu.Menu>
+    );
 
     return (
       <div>
         <Menu>
-        <Menu.Item
-          as={ Link }
-          to='/'
-          name='browse'
-          active={ activeItem === 'browse' }
-          onClick={ this.handleClick }
-        >
-          App
+          <Menu.Item
+            as={ Link }
+            to='/'
+            name='browse'
+            active={ activeItem === 'browse' }
+            onClick={ this.handleClick }>
+              App
         </Menu.Item>
-
-        <Menu.Menu position='right'>
-          <Menu.Item
-            as={ Link }
-            to='/signin'
-            name='signin'
-            active={ activeItem === 'signin' }
-            onClick={ this.handleClick }>
-            Sign In
-          </Menu.Item>
-
-          <Menu.Item
-            as={ Link }
-            to='/signup'
-            name='signup'
-            active={ activeItem === 'signup' }
-            onClick={ this.handleClick }>
-            Sign Up
-          </Menu.Item>
-        </Menu.Menu>
-      </Menu>
-
-      <Route path='/signup' component={ SignUpPage } />
-      <Route path='/signin' component={ SignInPage } />
+        { auth.isAuthenticated ? authed : unauthed }
+        </Menu>
+        <Route path='/signup' component={ SignUpPage } />
+        <Route path='/signin' component={ SignInPage } />
       </div>
     );
   }
 }
 
+NavMenu.propTypes = {
+  signOutUser: PropTypes.func.isRequired
+}
 
-export default NavMenu;
+const mapStateToProps = state => ({ auth: state.auth });
+
+export default connect(mapStateToProps, { signOutUser })(NavMenu);
